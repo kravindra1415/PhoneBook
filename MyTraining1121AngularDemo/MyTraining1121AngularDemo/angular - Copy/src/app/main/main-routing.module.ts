cd8074f0ea9a5e5e20ebaf395406cac266c82d06
kravindra1415 <kravindra1415@gmail.com>
2022-07-19 09:31:54 +0530

    initial commit

---
+++ MyTraining1121AngularDemo/MyTraining1121AngularDemo/angular - Copy/src/app/main/main-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    imports: [
+        RouterModule.forChild([
+            {
+                path: '',
+                children: [
+                    {
+                        path: 'dashboard',
+                        loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+                        data: { permission: 'Pages.Tenant.Dashboard' },
+                    },
+                    {
+                        path: 'phonebook',
+                        loadChildren: () => import('./phonebook/phonebook.module').then(m => m.PhoneBookModule),
+                        data: { permission: 'Pages.Tenant.PhoneBook' }
+                    }
+                    ,
+                    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+                    { path: '**', redirectTo: 'dashboard' },
+                ],
+            },
+        ]),
+    ],
+    exports: [RouterModule],
+})
+export class MainRoutingModule { }
